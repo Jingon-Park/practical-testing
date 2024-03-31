f873ea85e83a8f478b7f42fccc8cf025ef0a6884
gonee <bennp3038@gmail.com>
2024-03-31 16:37:20 +0900

    Presentation layer 테스트(2) Validate test 1

---
+++ src/test/java/sample/cafekiosk/spring/api/controller/product/ProductControllerTest.java
@@ -0,0 +1,92 @@
+package sample.cafekiosk.spring.api.controller.product;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.*;
+import static sample.cafekiosk.spring.domain.product.ProductType.*;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest.ProductCreateRequestBuilder;
+import sample.cafekiosk.spring.api.service.product.ProductService;
+import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
+import sample.cafekiosk.spring.domain.product.ProductType;
+
+@WebMvcTest(controllers = ProductController.class)
+class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private ProductService productService;
+
+    @Test
+    @DisplayName("신규 상품을 등록한다.")
+    void createProduct() throws Exception {
+
+        //given
+
+        ProductCreateRequest request = ProductCreateRequest.builder()
+            .type(HANDMADE)
+            .sellingStatus(SELLING)
+            .name("아메리카노")
+            .price(4000).build();
+
+
+        //when//then
+        mockMvc.perform(
+                post("/api/v1/product/new")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("신규 상품을 등록할 때에는 상품 타입은 필수값이다.")
+    void createProductWithOutType() throws Exception {
+
+        //given
+
+        ProductCreateRequest request = ProductCreateRequest.builder()
+            .sellingStatus(SELLING)
+            .name("아메리카노")
+            .price(4000).build();
+
+
+        //when//then
+        mockMvc.perform(
+                post("/api/v1/product/new")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.code").value("400"))
+            .andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.name()))
+            .andExpect(jsonPath("$.message").value("상품 타입은 필수입니다."));
+    }
+
+
+
+}
