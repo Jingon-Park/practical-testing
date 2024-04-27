5ffb260423694617f5314e4d6da5d2848b581e4a
gonee <bennp3038@gmail.com>
2024-04-27 16:37:48 +0900

    7-5 한눈에 들어오는 테스트 fixture 구성하기

--- src/test/java/sample/cafekiosk/spring/api/service/product/ProductServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/product/ProductServiceTest.java
@@ -6,6 +6,7 @@ import static sample.cafekiosk.spring.domain.product.ProductType.*;
 
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,22 @@ class ProductServiceTest {
     @Autowired
     private ProductRepository productRepository;
 
+    @BeforeEach
+    void setUp() {
+        /**
+         * 모든 테스트를 수행하기 전에 내용을 수행
+         * 자주 사용할 일이 없음
+         */
+
+        //사용하기전 체크 사항
+        // 각 테스트 코드의 입장에서 setUp내용을 알 필요가 없는지?
+        // 수정되더라도 모든 테스트 코드에 영향이 없는지??
+
+        /**
+         * 테스트 코드의 문서적인 관점에 봤을때, given절에 들어가는 Fixture 들이 많은 테스트 코드에서 곂치더라도
+         * 각 테스트 코드의 given절에 작성하는 것이 테스트 코드의 흐름을 이해하기에도 좋고 문서로의 역할도 수행하기에 좋음
+         */
+    }
     @AfterEach
     void tearDown() {
         this.productRepository.deleteAllInBatch();
